9fc64d4f5adea56b404c851ad9dc731d96c92e8a
John Lownie <jlownie@gmail.com>
2018-02-05 21:00:15 -0500

    Initial commit

--- PowerUp/src/RobotMap.h
+++ PowerUp/src/RobotMap.h
@@ -2,13 +2,16 @@
 #define _ROBOTMAP_HG_
 
 //Drive Train
-
 const int LEFT_FRONT_MOTOR_ID = 4;
 const int LEFT_REAR_MOTOR_ID = 3;
 const int RIGHT_FRONT_MOTOR_ID = 1;
 const int RIGHT_REAR_MOTOR_ID = 2;
 
 const double WHEEL_RADIUS = 3.0;
+
+const int SLOT_INDEX = 0;
+const int PID_LOOP_INDEX = 0;
+const int TIMEOUT_MS = 10;
 
 //XBox Controller
 const int XBOX_CONTROLLER_DRIVE_PORT = 0;
