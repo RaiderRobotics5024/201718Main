59be50df575c91960a9a21bc5457b650f0abc3e8
John Lownie <jlownie@gmail.com>
2018-02-06 19:39:30 -0500

    initial commit

--- PowerUp/src/RobotMap.h
+++ PowerUp/src/RobotMap.h
@@ -12,6 +12,9 @@ const double WHEEL_RADIUS = 3.0;
 const int SLOT_INDEX = 0;
 const int PID_LOOP_INDEX = 0;
 const int TIMEOUT_MS = 10;
+const int SENSOR_UNITS_PER_ROTATION = 4096;
+const int BASE_TRAJECTORY_PERIOD_MS = 0;
+const int NEUTRAL_DEADBAND_PERCENT = 1;
 
 //XBox Controller
 const int XBOX_CONTROLLER_DRIVE_PORT = 0;
