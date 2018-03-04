9cb180ca58b5d14bd1b5d3fef5d8968e43722440
Evan Pratten <ewpratten@gmail.com>
2018-03-04 16:04:33 -0500

    Update Log.h

--- PowerUp/src/Utilities/Log.h
+++ PowerUp/src/Utilities/Log.h
@@ -2,7 +2,8 @@
 #define _LOG_HG_
 
 #include <iostream>
+#include <chrono>
 
-#define LOG(x) { std::cout << x << std::endl; }
+#define LOG(x) { std::cout << "[ " << std::chrono::system_clock::now(); << " ] - " << x << std::endl; }
 
 #endif
