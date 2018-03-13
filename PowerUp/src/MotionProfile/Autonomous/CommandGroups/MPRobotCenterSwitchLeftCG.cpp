3cc1bcf599997a3ac219be4a99a11a9bfbd104f7
John Lownie <jlownie@rogers.com>
2018-03-13 11:51:03 -0400

    Update MPRobotCenterSwitchLeftCG.cpp

--- PowerUp/src/MotionProfile/Autonomous/CommandGroups/MPRobotCenterSwitchLeftCG.cpp
+++ PowerUp/src/MotionProfile/Autonomous/CommandGroups/MPRobotCenterSwitchLeftCG.cpp
@@ -14,35 +14,61 @@
  */
 MPRobotCenterSwitchLeftCG::MPRobotCenterSwitchLeftCG()
 {
-	// first cube
+	// FIRST CUBE
+	// move from robot center to switch left
+	// eject the cube
 	AddSequential(new RCtoSLCommand());
-	AddSequential(new ToggleIntake( -1.00));
+	AddSequential(new ToggleIntake(-1.00));
 
-	// second cube
+	// SECOND CUBE
+	// move from switch left back to robot center
+	// set elevator height to cube one
 	AddParallel(new SLtoRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::CUBE));
 
+	// move robot to pyramid 1 position
+	// open the gripper
 	AddParallel(new RCtoPY1Command());
 	AddSequential(new ToggleGripper(-1.00));
+	
+	// close the gripper
+	// intake the cube
+	AddParallel(new ToggleGripper(1.00));
 	AddSequential(new ToggleIntake ( 1.00));
 
+	// move robot back to robot center
+	// set elevator height to switch
 	AddParallel(new PY1toRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::SWITCH));
 
+	// move robot from robot center to switch left
+	// eject the cube
 	AddSequential(new RCtoSLCommand());
-	AddSequential(new ToggleIntake( -1.00));
+	AddSequential(new ToggleIntake(-1.00));
 
-	// third cube
+	// THIRD CUBE
+	// move robot from switch left back to robot center
+	// set elevator height to cube two
 	AddParallel(new SLtoRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::CUBEX2));
 
+	// move robot from robot center to pyramid two
+	// open gripper
 	AddParallel(new RCtoPY2Command());
 	AddSequential(new ToggleGripper(-1.00));
-	AddSequential(new ToggleIntake ( 1.00));
+		    
+	// close gripper
+	// intake the cube
+	AddParallel(new ToggleGripper(1.00));
+	AddSequential(new ToggleIntake (1.00));
 
+	// move robot back to robot center
+	// set elevator height to switch
 	AddParallel(new PY2toRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::SWITCH));
 
+	// move robot from robot center to switch left
+	// eject the cube
 	AddSequential(new RCtoSLCommand());
-	AddSequential(new ToggleIntake( -1.00));
+	AddSequential(new ToggleIntake(-1.00));
 }
