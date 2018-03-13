b77b0105486a8390870133811d34f2e89ab2e63f
John Lownie <jlownie@rogers.com>
2018-03-13 10:58:34 -0400

    Update MPRobotCenterSwitchRightCG.cpp

--- PowerUp/src/MotionProfile/Autonomous/CommandGroups/MPRobotCenterSwitchRightCG.cpp
+++ PowerUp/src/MotionProfile/Autonomous/CommandGroups/MPRobotCenterSwitchRightCG.cpp
@@ -14,35 +14,61 @@
  */
 MPRobotCenterSwitchRightCG::MPRobotCenterSwitchRightCG()
 {
-	 // first cube
+	// FIRST CUBE
+	// move from robot center to switch right
+	// eject the cube
 	AddSequential(new RCtoSRCommand());
 	AddSequential(new ToggleIntake( -1.00));
 
-	// second cube
+	// SECOND CUBE
+	// move from switch right to robot center
+	// move elevator to one cube height
 	AddParallel(new SRtoRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::CUBE));
 
+	// move to pyramid one position
+	// open gripper
 	AddParallel(new RCtoPY1Command());
 	AddSequential(new ToggleGripper(-1.00));
-	AddSequential(new ToggleIntake ( 1.00));
+	
+	// close gripper
+	// intake cube
+	AddParallel(new TogglerGripper(1.00));
+	AddSequential(new ToggleIntake (1.00));
 
+	// move back to robot center
+	// move elevator to switch position
 	AddParallel(new PY1toRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::SWITCH));
 
+	// move robot to switch right
+	// eject cube
 	AddSequential(new RCtoSRCommand());
 	AddSequential(new ToggleIntake( -1.00));
 
-	// third cube
+	// THIRD CUBE
+	// move back to robot center
+	// move elevator to two cube height
 	AddParallel(new SRtoRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::CUBEX2));
 
+	// move to pyramid two position
+	// open gripper
 	AddParallel(new RCtoPY2Command());
 	AddSequential(new ToggleGripper(-1.00));
-	AddSequential(new ToggleIntake ( 1.00));
+	
+	// close gripper
+	// intake cube
+	AddParallel(new ToggleGripper(1.00));
+	AddSequential(new ToggleIntake (1.00));
 
+	// move back to robot center
+	// move elevator to switch height
 	AddParallel(new PY2toRCCommand());
 	AddSequential(new MoveElevator(Elevator::Positions::SWITCH));
 
+	// move robot to switch right
+	// eject cube
 	AddSequential(new RCtoSRCommand());
 	AddSequential(new ToggleIntake( -1.00));
 }
