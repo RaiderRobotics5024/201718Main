cd0cd1d0ffedbceefa304d99a5eff35dbe68be8d
John Lownie <jlownie@rogers.com>
2018-03-28 11:22:13 -0400

    Update DriveTrainMap.h

--- PowerUp/src/Subsystems/DriveTrainMap.h
+++ PowerUp/src/Subsystems/DriveTrainMap.h
@@ -5,29 +5,37 @@
 
 const bool IS_QBERT = true; // change this to false if using the test bot
 
+// Motor IDs
 const int DRIVETRAIN_LEFT_FRONT_MOTOR_ID = 1;
 const int DRIVETRAIN_LEFT_REAR_MOTOR_ID = 2;
 const int DRIVETRAIN_RIGHT_FRONT_MOTOR_ID = 3;
 const int DRIVETRAIN_RIGHT_REAR_MOTOR_ID = 4;
 
-const double WHEEL_RADIUS = 3.0;
+// Robot Specifications
+const double ROBOT_LENGTH = 38.00;
+const double ROBOT_WIDTH = 32.00;
 const double WHEEL_TO_WHEEL = 19.75;
-const double HALF_ROBOT_LENGTH = 19.00;
+const double WHEEL_RADIUS = 3.0;
+
+// Encoder Settings
 const int TICKS_PER_REVOLUTION = 1440; //2048; //4096;
 const double INCHES_PER_REVOLUTION = 2.0 * M_PI * WHEEL_RADIUS;
 const double INCHES_PER_TICK = INCHES_PER_REVOLUTION / TICKS_PER_REVOLUTION;
+
+// Talon Mode Settings
 const int SLOT_INDEX = 0;
 const int PID_LOOP_INDEX = 0;
 const int TIMEOUT_MS = 100;
 const int BASE_TRAJECTORY_PERIOD_MS = 0;
 const int NEUTRAL_DEADBAND_PERCENT = 1;
 
+// Talon PIDF Settings
 const double TALON_PID_P = 0.03f;
 const double TALON_PID_I = 0.00;
 const double TALON_PID_D = 0.00;
 const double TALON_PID_F = 0.00;
 
-//Gyro Settings:
+//Gyro PIDF Settings:
 const static double GYRO_PID_P = 0.005f;
 const static double GYRO_PID_I = 0.004f;
 const static double GYRO_PID_D = 0.002f;
