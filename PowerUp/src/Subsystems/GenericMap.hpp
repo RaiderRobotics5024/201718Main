7dffeb2d8037d8daea8d83b7e8f225ebc9bdb16c
Evan Pratten <ewpratten@gmail.com>
2018-05-23 17:11:44 -0400

    Added slow and reverse to trigger drive. Started working on GenericControl

---
+++ PowerUp/src/Subsystems/GenericMap.hpp
@@ -0,0 +1,19 @@
+#ifndef _DRIVE_TRAIN_MAP_HG_
+#define _DRIVE_TRAIN_MAP_HG_
+
+#include <math.h>
+#include "../RobotCFG.hpp"
+
+// Motor IDs
+#define PROTO_1_MOTOR_ID 5
+#define PROTO_2_MOTOR_ID 6
+
+
+// Talon Mode Settings
+#define SLOT_INDEX 0
+#define PID_LOOP_INDEX 0
+#define TIMEOUT_MS 100
+#define BASE_TRAJECTORY_PERIOD_MS 0
+#define NEUTRAL_DEADBAND_PERCENT 1
+
+#endif
